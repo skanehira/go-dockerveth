1e7d1e081800a4d59d388e600915353e7073bed5
skanehira <sho19921005@gmail.com>
2020-02-11 02:01:49 +0900

    mapping docker veth

--- docker.go
+++ docker.go
@@ -1,25 +1,82 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"fmt"
+	"errors"
+	"strconv"
+	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 )
 
-func containers() {
-	cli, err := client.NewEnvClient()
+var (
+	ErrEmptyExecID = errors.New("empty exec id")
+)
+
+// Client wrap docker client
+type Client struct {
+	*client.Client
+}
+
+// NewClient create new docker client
+func NewClient() (*Client, error) {
+	c, err := client.NewEnvClient()
 	if err != nil {
-		panic(err)
+		return nil, err
+	}
+	return &Client{c}, nil
+}
+
+// Containers get contianers
+func (cli *Client) Containers() ([]types.Container, error) {
+	return cli.ContainerList(context.Background(), types.ContainerListOptions{})
+}
+
+// ContainerPID get container process id on host os
+// cid: container id
+func (cli *Client) ContainerPID(cid string) (string, error) {
+	containerJSON, err := cli.ContainerInspect(context.Background(), cid)
+	if err != nil {
+		return "", err
+	}
+	return strconv.Itoa(containerJSON.State.Pid), nil
+}
+
+// ContainerIflink get container iflink
+func (cli *Client) ContainerIflink(cid string) (string, error) {
+	res, err := cli.ContainerExecCreate(context.Background(), cid, types.ExecConfig{
+		AttachStdin:  true,
+		AttachStdout: true,
+		AttachStderr: true,
+		Cmd:          []string{"cat", "/sys/class/net/eth0/iflink"},
+	})
+
+	if err != nil {
+		return "", err
+	}
+	if res.ID == "" {
+		return "", ErrEmptyExecID
 	}
 
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	hijackResponse, err := cli.ContainerExecAttach(context.Background(), res.ID, types.ExecConfig{})
 	if err != nil {
-		panic(err)
+		return "", err
+	}
+	defer hijackResponse.Close()
+
+	var dstout, dsterr = &bytes.Buffer{}, &bytes.Buffer{}
+	_, err = stdcopy.StdCopy(dstout, dsterr, hijackResponse.Reader)
+	if err != nil {
+		return "", err
 	}
 
-	for _, container := range containers {
-		fmt.Printf("%s %s %s\n", container.ID[:10], container.Image, container.Names)
+	// if exec command return error
+	if dsterr.Len() != 0 {
+		return "", errors.New(dsterr.String())
 	}
+
+	return strings.Trim(dstout.String(), "\n"), nil
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -21,18 +22,59 @@ func getInterfaces() (nets []net.Interface, err error) {
 	return
 }
 
+func fileExists(name string) bool {
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
 func onExit(err error) {
 	fmt.Fprintln(os.Stderr, err)
 	os.Exit(1)
 }
 
-func main() {
-	nets, err := getInterfaces()
+func run() error {
+	cli, err := NewClient()
 	if err != nil {
-		onExit(err)
+		return err
 	}
 
-	containers()
+	nets, err := getInterfaces()
+	if err != nil {
+		return err
+	}
 
-	fmt.Println(nets)
+	containers, err := cli.Containers()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("CONTAINER\tVETH\t\tNAMES")
+
+	for _, c := range containers {
+		iflink, err := cli.ContainerIflink(c.ID)
+		if err != nil {
+			return err
+		}
+
+		ifindex, err := strconv.Atoi(iflink)
+		if err != nil {
+			return err
+		}
+
+		for _, i := range nets {
+			if ifindex == i.Index {
+				fmt.Printf("%s\t%s\t%s\t\n", c.ID[:10], i.Name, c.Names)
+			}
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		onExit(err)
+	}
 }
